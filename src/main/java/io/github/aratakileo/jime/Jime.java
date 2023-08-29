5420eaae984155046df78a6ec06547b741debd12
AratakiLeo <83653555+aratakileo@users.noreply.github.com>
2023-08-29 17:54:19 +0300

    Move suggestions functionality to suggestions API
    It is necessary to avoid conflicts with suggestions with the Emogg mod

--- src/main/java/io/github/aratakileo/jime/Jime.java
+++ src/main/java/io/github/aratakileo/jime/Jime.java
@@ -1,12 +1,51 @@
 package io.github.aratakileo.jime;
 
+import io.github.aratakileo.jime.converter.HiraganaConverter;
+import io.github.aratakileo.jime.converter.KanjiConverter;
+import io.github.aratakileo.suggestionsapi.SuggestionsAPI;
+import io.github.aratakileo.suggestionsapi.suggestion.AlwaysShownSuggestion;
+import io.github.aratakileo.suggestionsapi.suggestion.SimpleSuggestionsInjector;
+import io.github.aratakileo.suggestionsapi.suggestion.Suggestion;
 import net.fabricmc.api.ClientModInitializer;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public class Jime implements ClientModInitializer {
     public final static Logger LOGGER = LoggerFactory.getLogger(Jime.class);
 
     @Override
-    public void onInitializeClient() {}
+    public void onInitializeClient() {
+        SuggestionsAPI.registerSuggestionsInjector(
+                new SimpleSuggestionsInjector(Pattern.compile("[A-Za-z0-9]+")) {
+                    @Override
+                    public boolean isIsolated() {
+                        return true;
+                    }
+
+                    @Override
+                    public <T extends Suggestion> List<T> getUncheckedSuggestions(
+                            @NotNull String currentExpression
+                    ) {
+                        final var hiraganaLiterals = HiraganaConverter.convert(
+                                currentExpression.substring(getStartOffset())
+                        );
+                        final var output = new ArrayList<Suggestion>(
+                                KanjiConverter.convert(hiraganaLiterals)
+                                        .stream()
+                                        .map(AlwaysShownSuggestion::new)
+                                        .toList()
+                        );
+
+                        output.add(new AlwaysShownSuggestion(hiraganaLiterals));
+
+                        return (List<T>) output;
+                    }
+                }
+        );
+    }
 }
