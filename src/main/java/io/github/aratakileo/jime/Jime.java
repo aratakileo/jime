b041fab1232e997126e05af55a9fc7913ae135b0
AratakiLeo <83653555+aratakileo@users.noreply.github.com>
2023-08-31 22:01:48 +0300

    Add asynchrony for network suggestions
    Now there are no friezes when displaying suggestions in kanji

--- src/main/java/io/github/aratakileo/jime/Jime.java
+++ src/main/java/io/github/aratakileo/jime/Jime.java
@@ -1,15 +1,16 @@
 package io.github.aratakileo.jime;
 
+import com.google.common.collect.Lists;
 import io.github.aratakileo.jime.converter.HiraganaConverter;
 import io.github.aratakileo.jime.converter.KanjiConverter;
 import io.github.aratakileo.suggestionsapi.SuggestionsAPI;
+import io.github.aratakileo.suggestionsapi.suggestion.Injector;
 import io.github.aratakileo.suggestionsapi.suggestion.Suggestion;
-import io.github.aratakileo.suggestionsapi.suggestion.SuggestionsInjector;
+import io.github.aratakileo.suggestionsapi.util.Cast;
 import net.fabricmc.api.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 public class Jime implements ClientModInitializer {
@@ -17,30 +18,39 @@ public class Jime implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        SuggestionsAPI.registerSuggestionsInjector(SuggestionsInjector.simple(
+        SuggestionsAPI.registerSuggestionsInjector(Injector.simple(
                 Pattern.compile("[A-Za-z0-9]+"),
-                (currentExpression, offsettedExpression) -> {
-                    final var hiraganaLiterals = HiraganaConverter.convert(offsettedExpression);
-                    final var output = new ArrayList<Suggestion>(
-                            KanjiConverter.convert(hiraganaLiterals)
+                (currentExpression, startOffset) -> Lists.newArrayList(
+                        Suggestion.alwaysShown(
+                                HiraganaConverter.convert(
+                                        currentExpression.substring(startOffset).toLowerCase()
+                                )
+                        )
+                ),
+                true
+        ));
+
+        SuggestionsAPI.registerSuggestionsInjector(Injector.async(
+                Pattern.compile("[A-Za-z0-9]+"),
+                (currentExpression, startOffset, applier) -> () -> {
+                    applier.accept(Lists.newArrayList(
+                            KanjiConverter.convert(HiraganaConverter.convert(currentExpression.substring(startOffset)))
                                     .stream()
                                     .map(Suggestion::alwaysShown)
                                     .toList()
-                    );
-
-                    output.add(Suggestion.alwaysShown(hiraganaLiterals));
-
-                    return output;
+                    ));
                 },
                 true
         ));
 
-        SuggestionsAPI.registerSuggestionsInjector(SuggestionsInjector.simple(
+        SuggestionsAPI.registerSuggestionsInjector(Injector.simple(
                 Pattern.compile("\\.{3}|[-,.?!<>(){}&\"'\\[\\]]"),
-                (currentExpression, offsettedExpression) -> HiraganaConverter.getVariations(offsettedExpression)
-                        .stream()
-                        .map(Suggestion::alwaysShown)
-                        .toList(),
+                (currentExpression, startOffset) -> Cast.of(
+                        HiraganaConverter.getVariations(currentExpression.substring(startOffset))
+                                .stream()
+                                .map(Suggestion::alwaysShown)
+                                .toList()
+                ),
                 true
         ));
     }
