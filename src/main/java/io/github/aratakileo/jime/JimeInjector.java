26fbe6e424f0b017f73f1ebc34a201372ed9d97c
AratakiLeo <83653555+aratakileo@users.noreply.github.com>
2024-01-03 15:27:09 +0300

    Restructure code

--- src/main/java/io/github/aratakileo/jime/JimeInjector.java
+++ src/main/java/io/github/aratakileo/jime/JimeInjector.java
@@ -1,6 +1,6 @@
 package io.github.aratakileo.jime;
 
-import com.google.common.collect.Lists;
+import com.google.common.collect.Iterables;
 import io.github.aratakileo.jime.converter.HiraganaConverter;
 import io.github.aratakileo.jime.converter.ImeClient;
 import io.github.aratakileo.suggestionsapi.injector.AsyncInjector;
@@ -11,53 +11,44 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Supplier;
-import java.util.regex.Pattern;
 
 public class JimeInjector implements SuggestionsInjector, AsyncInjector {
-    private final static Pattern PATTERN = Pattern.compile("[A-Za-z0-9]+");
-
     private int startOffset = 0;
     private boolean shouldShowSuggestions = true;
 
     @Override
-    public @Nullable Supplier<@Nullable List<Suggestion>> getAsyncApplier(@NotNull String currentExpression) {
-        if (!shouldShowSuggestions) return null;
-
-        return () -> Lists.newArrayList(
-                ImeClient.getKanjiVariations(HiraganaConverter.convert(currentExpression.substring(startOffset)))
-                        .stream()
-                        .map(Suggestion::alwaysShown)
-                        .toList()
-        );
-    }
-
-    @Override
     public @Nullable List<Suggestion> getSuggestions(@NotNull String currentExpression) {
-        int lastMatchedStart = Injector.getLastMatchedStart(PATTERN, currentExpression);
+        int lastMatchedStart = Injector.getLastMatchedStart(Injector.SIMPLE_WORD_PATTERN, currentExpression);
 
         shouldShowSuggestions = false;
 
         if (lastMatchedStart == -1) return null;
 
         shouldShowSuggestions = true;
-
         startOffset = lastMatchedStart;
 
-        return Lists.newArrayList(
-                Suggestion.alwaysShown(
-                        HiraganaConverter.convert(currentExpression.substring(lastMatchedStart).toLowerCase())
-                )
-        );
+        return List.of(Suggestion.alwaysShown(
+                HiraganaConverter.convert(currentExpression.substring(lastMatchedStart).toLowerCase())
+        ));
+    }
+
+    @Override
+    public @Nullable Supplier<@Nullable List<Suggestion>> getAsyncApplier(@NotNull String currentExpression) {
+        if (!shouldShowSuggestions) return null;
+
+        return () -> {
+            final var answeredHashMap = ImeClient.getRequestAnswer(currentExpression.substring(startOffset));
+
+            if (Objects.isNull(answeredHashMap)) return List.of();
+
+            return Iterables.getLast(answeredHashMap.values()).stream().map(Suggestion::alwaysShown).toList();
+        };
     }
 
     @Override
     public int getStartOffset() {
         return startOffset;
-    }
-
-    @Override
-    public boolean isIsolated() {
-        return true;
     }
 }
--- src/main/java/io/github/aratakileo/jime/converter/ImeClient.java
+++ src/main/java/io/github/aratakileo/jime/converter/ImeClient.java
@@ -4,44 +4,59 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import io.github.aratakileo.jime.Jime;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Objects;
 
 public class ImeClient {
     private static final String BASE_URL = "https://www.google.com/transliterate?langpair=ja-Hira%7Cja&text=";
 
-    public static @NotNull List<String> getKanjiVariations(@NotNull String hiragana) {
-        if (hiragana.isEmpty())
-            return List.of();
+    public static @Nullable LinkedHashMap<@NotNull String, @NotNull List<@NotNull String>> getRequestAnswer(
+            @NotNull String romajiText
+    ) {
+        final var hiraganaText = HiraganaConverter.convert(romajiText.toLowerCase());
 
-        BufferedReader reader = null;
+        if (hiraganaText.isEmpty())
+            return null;
+
+        var reader = (BufferedReader) null;
 
         try {
-            final var url = new URI(BASE_URL + URLEncoder.encode(hiragana, StandardCharsets.UTF_8)).toURL();
+            final var url = new URI(BASE_URL + URLEncoder.encode(hiraganaText, StandardCharsets.UTF_8)).toURL();
 
             reader = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8));
 
-            return JsonParser.parseReader(reader)
-                    .getAsJsonArray()
-                    .get(0)
-                    .getAsJsonArray()
-                    .get(1)
-                    .getAsJsonArray()
-                    .asList()
-                    .stream()
-                    .map(JsonElement::getAsString)
-                    .toList();
+            final var finalReader = reader;
+
+            return new LinkedHashMap<>() {{
+                JsonParser.parseReader(finalReader)
+                        .getAsJsonArray()
+                        .asList()
+                        .stream()
+                        .map(JsonElement::getAsJsonArray)
+                        .forEach(untransformedEntry -> put(
+                                untransformedEntry.get(0).getAsString(),
+                                untransformedEntry.get(1)
+                                        .getAsJsonArray()
+                                        .asList()
+                                        .stream()
+                                        .map(JsonElement::getAsString)
+                                        .toList()
+                        ));
+            }};
         } catch (Exception e) {
-            Jime.LOGGER.error("KanjiConverter error: ", e);
+            Jime.LOGGER.error("ImeClient request error: ", e);
         } finally {
-            if (reader != null) try {reader.close();} catch (IOException ignore) {}
+            if (Objects.nonNull(reader)) try {reader.close();} catch (IOException ignore) {}
         }
 
-        return List.of();
+        return null;
     }
 }
