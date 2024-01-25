8fd77130968c7f7a8a48dee97eed4f61ee250f2e
AratakiLeo <83653555+aratakileo@users.noreply.github.com>
2024-01-25 16:31:09 +0300

    Fix crash when special literals entered

--- src/main/java/io/github/aratakileo/jime/Jime.java
+++ src/main/java/io/github/aratakileo/jime/Jime.java
@@ -1,15 +1,9 @@
 package io.github.aratakileo.jime;
 
-import io.github.aratakileo.jime.converter.HiraganaConverter;
 import io.github.aratakileo.suggestionsapi.SuggestionsAPI;
-import io.github.aratakileo.suggestionsapi.injector.Injector;
-import io.github.aratakileo.suggestionsapi.suggestion.Suggestion;
-import io.github.aratakileo.suggestionsapi.util.Cast;
 import net.fabricmc.api.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.regex.Pattern;
 
 public class Jime implements ClientModInitializer {
     public final static Logger LOGGER = LoggerFactory.getLogger(Jime.class);
@@ -17,15 +11,5 @@ public class Jime implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         SuggestionsAPI.registerInjector(new JimeInjector());
-
-        SuggestionsAPI.registerInjector(Injector.simple(
-                Pattern.compile("\\.{3}|[-,.?!<>(){}&\"'\\[\\]]"),
-                (stringContainer, startOffset) -> Cast.of(
-                        HiraganaConverter.getVariations(stringContainer.getContent().substring(startOffset))
-                                .stream()
-                                .map(Suggestion::alwaysShown)
-                                .toList()
-                )
-        ));
     }
 }
